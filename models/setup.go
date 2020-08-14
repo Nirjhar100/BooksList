d21e39a66da911a2aff498ab24263dd6b4bcdfd1
Nirjhar Debnath <nirjhar@Nirjhars-MacBook-Air.local>
2020-08-14 05:53:06 +0530

    first-commit

---
+++ models/setup.go
@@ -0,0 +1,19 @@
+package models
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDataBase() {
+	database, err := gorm.Open("sqlite3", "test.db")
+
+	if err != nil {
+		panic("Failed to connect to database")
+	}
+
+	database.AutoMigrate(&Book{})
+
+	DB = database
+}
